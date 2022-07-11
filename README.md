766df5af83c9fdead92ea4eb6e7e7910be24bfbc
misaka-10031 <goodbird@yuban.tech>
2022-07-12 07:26:40 +0800

    应该没问题了吧:P

--- README.md
+++ README.md
@@ -12,13 +12,13 @@
 git clone 'https://github.com/ChinaMisakaNetwork/Index-training.git'  
 cd Index-training  
 git clone 'https://github.com/1204244136/index-X'  
-python main.py # 还没想好些什么就先这样写吧  
+python main.py/python3 main.py  
 ```
 
 ### 文件说明
 
-将在index-X目录下生成`index.txt`文件，此文件为原始文本。  
-fenci.py会删除换行，并对文本进行分词。  
+将在根目录下生成`index.txt`文件，此文件为原始文本。  
+根目录下生成`index-cut.txt`，此文件为分词后的文本。  
 
 ### 语料库格式
 
@@ -29,7 +29,10 @@ TODO
 #### V1.0
 
 1. 将魔法禁书目录X系列的小说 `.epub` 转换为 `.txt` 格式  
-2. 使用jieba分词工具分词  
+2. 提取专有名词，建立词库  
+3. 使用jieba分词工具分词  
+
+> 已完成，但效果不是很好，待优化...
 
 #### V2.0
 
--- fenci.py
+++ fenci.py
@@ -4,15 +4,16 @@ from tqdm import tqdm
 from names import get_name,get_stop
 
 #将小说角色名、专有名词载入jieba词库:专有名词生成见src/name-process/name.py
-names = get_name()
-for word in names:
-    jieba.add_word(word)
-stop_words = get_stop()
 
 def cutIt(inputFileName,outputFileName):
     output = open(outputFileName,'w',encoding='utf8')
     finish = 0
     seg_list = jieba.cut("正在加载分词数据")  # 避免进度条被阻断
+    # 导入专有名词与停用词
+    names = get_name()
+    for word in names:
+        jieba.add_word(word)
+    stop_words = get_stop()
     print(" ".join(seg_list))
     with open(inputFileName,'r',encoding='utf8') as lines:
         print ('正在载入文件...')
@@ -30,6 +31,6 @@ def cutIt(inputFileName,outputFileName):
                 output.write(sentence)
                 line = source.readline()
                 for stop_word in stop_words:
-                    text_sub = text_sub.replace(stop_word, " ")
+                    line = line.replace(stop_word, "")
                 pbar.update(1)
         print('全部完成，共处理了',str(count),'条')
